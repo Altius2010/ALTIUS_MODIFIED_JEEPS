b184c468d298735b0a4b2cb4d60ceba0c660f3b2
Altius2010 <72147694+Altius2010@users.noreply.github.com>
2021-04-08 18:18:23 +0530

    Add files via upload

---
+++ AMJ.py
@@ -0,0 +1,11 @@
+print("ALTIUS MODIFIED JEEPS", end = '     ')
+print("Contact us on: +91829055397", end = '       ')
+print("To view jeeps and place your order visit: www.modifiedjeeps.com")
+print("Website and app designed by Prabhnoor Singh Jeeps made by Sukhpal Singh")
+print("Description: We make brand-new jeeps according to customers choice we are from Mandi Dabwali-based company we")
+print("customise jeeps in brand-new condition short chases long chasesWe will make modified jeeps as Willis MM 550 Jeep Gypsy one ton Jaunga everything we customise thank you.")
+rating = input("please give us rating good or bad: ")
+if rating.lower() == "good":
+        print("Thank you")
+elif rating.lower() == "bad":
+        print("Sorry for that")
